903710106bd3b21873ef4fe2e29beda2282eaa0c
Adib <Adibvafa.fallahpour@mail.utoronto.ca>
2023-08-02 17:23:37 -0400

    Primary

--- streamlit.py
+++ streamlit.py
@@ -245,13 +245,12 @@ def main():
 
                 job_posting_input = st.text_area("Job Posting...")
 
-                # Custom button to submit the job posting
                 if st.button("Analyze Job Posting"):
                     st.session_state.messages.append({"role": "user", "content": job_posting_input})
                     with st.chat_message("user"):
                         st.markdown(job_posting_input)
 
-                    job_posting_summary = get_job_posting_summary(job_posting_input)
+                    job_posting_summary = get_job_posting(job_posting_input)
 
                     with st.chat_message("assistant"):
                         st.markdown(f"Job Posting Analyzed! Here is the summary: {job_posting_summary}")
@@ -294,9 +293,9 @@ def main():
 
             ending_message = intro_message_2
 
-            with st.chat_message("assistant"):
-                st.markdown(ending_message)
-            st.session_state.messages.append({"role": "assistant", "content": ending_message})
+            # with st.chat_message("assistant"):
+            #     st.markdown(ending_message)
+            # st.session_state.messages.append({"role": "assistant", "content": ending_message})
 
         else:
             reset_prompt_message = "Please only send Q, I, C, or J."
